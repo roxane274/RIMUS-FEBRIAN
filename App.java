3ff6b43a6a78d150518bf66ff187cd0db9a76ab7
roxane274 <106162262+roxane274@users.noreply.github.com>
2022-05-24 22:19:37 +0800

    Add files via upload
    210250502048-RIMUS-FEBRIAN-RANK-MOBILE-LEGENDS

---
+++ App.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.rimusfebrian.tugas4;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class App {
+
+    public static void main(String[] args) {
+    int rankmytic = 450;
+    if (rankmytic < 300){
+        System.out.println("mytic IV ");
+    }else if (rankmytic < 400){
+        System.out.println("mytic III ");
+    }else if (rankmytic < 500){
+        System.out.println("mytic II ");
+    }else {
+        System.out.println("mytic I ");
+    }
+    }
+}
